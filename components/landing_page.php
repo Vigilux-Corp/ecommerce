531d64e2a909789036fc9222540158e921410523
albanusCode <112880584+albanusCode@users.noreply.github.com>
2023-11-18 16:07:01 +0300

    user_flow page 1

--- components/landing_page.php
+++ components/landing_page.php
@@ -238,7 +238,6 @@
         document.getElementById("myDropdown").classList.toggle("show");
         }
 
-        // Close the dropdown menu if the user clicks outside of it
         window.onclick = function(event) {
         if (!event.target.matches('.dropbtn')) {
             var dropdowns = document.getElementsByClassName("dropdown-content1");
